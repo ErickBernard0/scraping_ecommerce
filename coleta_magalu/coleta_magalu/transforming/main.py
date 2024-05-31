be8ba9cb83926ce9e8de7fa4765aad5cc8cb0a87
Erick Bernardo <contato.bernardo25@gmail.com>
2024-05-31 13:20:24 -0300

    commit coleta_magalu

---
+++ coleta_magalu/coleta_magalu/transforming/main.py
@@ -0,0 +1,23 @@
+
+# importing libs
+import pandas as pd
+import duckdb
+from datetime import datetime
+
+# reading file
+data_path = '..\data\data_magalu.jsonl'
+df_data = pd.read_json(data_path, lines=True)
+
+# data transformation
+df_data['dt_coleta'] = datetime.now()
+df_data['price_of'] = df_data['price_of'].str.replace('R$\xa0', '').str.replace(',', '.').fillna(0)
+df_data['price_per'] = df_data['price_per'].str.replace('R$\xa0', '').str.replace(',', '.').fillna(0)
+
+# connect to db
+conn = duckdb.connect(database='../data/magalu.db', read_only=False)
+
+# save data to db
+df_data.to_sql('products_magalu', conn, if_exists='replace', index=False)
+
+# close conection to db
+conn.close()
